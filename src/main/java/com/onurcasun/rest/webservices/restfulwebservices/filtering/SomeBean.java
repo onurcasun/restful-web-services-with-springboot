800e3bb12998f527c2fb7074291093a185c5b61d
Onur Casun <onurcasun@outlook.com>
2021-06-04 00:21:21 +0300

    step 38, filtering some properties to don't show to the user every line of data :)

---
+++ src/main/java/com/onurcasun/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,44 @@
+package com.onurcasun.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = {"field1"}) // another approach to ignore mapping.
+public class SomeBean {
+    private String field1;
+    private String field2;
+
+    @JsonIgnore
+    private String field3; //will be ignored on mapping and will not show the user.
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return this.field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return this.field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return this.field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+}
